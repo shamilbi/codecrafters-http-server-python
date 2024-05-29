007ab51df957f0059b1c650b91eba41f4989577f
shamilbi <shamilbi@gmail.com>
2024-05-29 18:05:10 +0200

    return status

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ import socket
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, _ = server_socket.accept()    # wait for client
+    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
