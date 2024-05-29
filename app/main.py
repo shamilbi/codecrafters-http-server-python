a2c5889f3e61d60182cfb4a2c44ce2404858a70e
shamilbi <shamilbi@gmail.com>
2024-05-29 21:55:50 +0200

    url=/user-agent

--- app/main.py
+++ app/main.py
@@ -21,28 +21,64 @@ def read_lines(f_recv):
         yield rest.decode()
 
 
+def parse_request(conn):
+    d = {}
+    headers = []
+    headersD = {}
+    body = []
+    target = 0  # request
+    for line in read_lines(conn.recv):
+        if target == 0:
+            # GET URL HTTP
+            d['request'] = line
+            l = line.split()
+            d['url'] = l[1]
+            target = 1  # fields
+        elif target == 1:
+            if line:
+                l = line.split(':', maxsplit=1)
+                field = l[0]
+                value = l[1].strip()
+                headers.append((field, value))
+                headersD[field.lower()] = value
+                #print(f'field {field}: {value}')
+            else:
+                target = 2
+        elif target == 2:
+            if not line:
+                break
+            body.append(line)
+    d['headers'] = headers
+    d['headersD'] = headersD
+    d['body'] = body
+    return d
+
+
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     while True:
         conn, _ = server_socket.accept()    # wait for client
         with conn:
-            for i, line in enumerate(read_lines(conn.recv)):
-                if i == 0:
-                    # GET URL HTTP
-                    l = line.split()
-                    url = l[1]
-                    if url == '/':
-                        conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-                    elif url.startswith('/echo/'):
-                        body = url[6:].encode()
-                        conn.send(b'HTTP/1.1 200 OK\r\n')
-                        conn.send(b'Content-Type: text/plain\r\n')
-                        conn.send(f'Content-Length: {len(body)}\r\n'.encode())
-                        conn.send(b'\r\n')
-                        conn.send(body)
-                    else:
-                        conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
-                break
+            d = parse_request(conn)
+            url = d['url']
+            if url == '/':
+                conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+            elif url.startswith('/echo/'):
+                body = url[6:].encode()
+                conn.send(b'HTTP/1.1 200 OK\r\n')
+                conn.send(b'Content-Type: text/plain\r\n')
+                conn.send(f'Content-Length: {len(body)}\r\n'.encode())
+                conn.send(b'\r\n')
+                conn.send(body)
+            elif url == '/user-agent':
+                body = d['headersD']['user-agent'].encode()
+                conn.send(b'HTTP/1.1 200 OK\r\n')
+                conn.send(b'Content-Type: text/plain\r\n')
+                conn.send(f'Content-Length: {len(body)}\r\n'.encode())
+                conn.send(b'\r\n')
+                conn.send(body)
+            else:
+                conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
 
 if __name__ == "__main__":
